9b247dddf4d465e35d759783a455527596d81e4c
MuminjonN <m.o.murodullayev@gmail.com>
2024-11-02 12:40:18 +0500

    2ta son uchun ekub topish

--- DarsTMI/Program.cs
+++ DarsTMI/Program.cs
@@ -10,6 +10,18 @@ namespace DarsTMI
     {
         static void Main(string[] args)
         {
+            int a = 24, b = 25;
+
+            while(a!=b)
+            {
+                if (a > b)
+                    a = a - b;
+                else
+                    b= b - a;   
+            }
+            Console.WriteLine("ekub = "+ a);
+            Console.ReadKey();  
+
         }
     }
 }
